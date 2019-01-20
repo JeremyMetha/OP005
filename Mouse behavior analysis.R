96710c09a0a4d408e82d29cc550f4908273725b4
Jeremy Metha <methaj@3330D-133694-M.local>
2019-01-21 09:30:51 +1100

    It works! does it same my userdata too?

--- Mouse behavior analysis.R
+++ Mouse behavior analysis.R
@@ -230,5 +230,3 @@ justWTRRatios <- ratioPresses %>%
 #' matched with animal IDs is the number of days it took them to reach criterion in the WTL learning phase - this may be a proxy for intellegence
 #'
 #' Top-level: Drug group to which animal is assigned - this is the effct we want to look at
-
-
